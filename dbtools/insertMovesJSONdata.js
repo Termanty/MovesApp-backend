12d294baa93ad41305330dc7c19158528bf657ff
Tero Mäntylä <termanty@gmail.com>
2022-03-17 09:55:19 +0200

    fixing identation

--- dbtools/insertMovesJSONdata.js
+++ dbtools/insertMovesJSONdata.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // INSTRUCTIONS:
 // This is a tool to import the moves from db.json to our mySql database
@@ -14,39 +14,39 @@
 // 	},
 // ]
 
-const MariaDb = require('../dist/mariaDB');
-const jsondb = require('../db.json');
-require('dotenv').config();
+const MariaDb = require("../dist/mariaDB");
+const jsondb = require("../db.json");
+require("dotenv").config();
 
 const options = {
-	host: process.env.DB_HOST || '',
-	port: process.env.DB_PORT || '',
-	user: process.env.DB_USER || '',
-	password: process.env.DB_PASSWORD || '',
-	database: process.env.DB_DATABASE || '',
+  host: process.env.DB_HOST || "",
+  port: process.env.DB_PORT || "",
+  user: process.env.DB_USER || "",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "",
 };
 
 const db = new MariaDb(options);
 
 async function add(moves) {
-	for (let move of moves) {
-		try {
-			const parameters = [
-				move.Id,
-				move.Move,
-				move.Creator === '?' ? null : move.Creator || null,
-				move.HOX || null,
-				move.Link || null,
-			];
+  for (let move of moves) {
+    try {
+      const parameters = [
+        move.Id,
+        move.Move,
+        move.Creator === "?" ? null : move.Creator || null,
+        move.HOX || null,
+        move.Link || null,
+      ];
 
-			const sql =
-				'insert into moves (id,movename,creator,hox,link) values(?,?,?,?,?)';
+      const sql =
+        "insert into moves (id,movename,creator,hox,link) values(?,?,?,?,?)";
 
-			const status = await db.doQuery(sql, parameters);
-			console.log('Status', status);
-		} catch (err) {
-			console.log(err);
-		}
-	}
+      const status = await db.doQuery(sql, parameters);
+      console.log("Status", status);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
 add(jsondb);
